89c643d0783380e7bc1b4f44478cf6e366dd739e
Joseph Boyd <josephboyd9@gmail.com>
2016-09-03 19:47:48 +1200

    about to try attempt 1 to perform connection.

--- SimpleServer.java
+++ SimpleServer.java
@@ -4,15 +4,17 @@ import java.io.*;
 class SimpleServer{
     public static void main(String[] args){
 	try {
-	    ServerSocket ss = new ServerSocket(8080);
+	    ServerSocket ss = new ServerSocket(0);
 	    while(true) {
 		Socket client = ss.accept();
-		PrintWriter writer = new
-		    PrintWriter(client.getOutputStream(), true);
-		BufferedReader reader = new
-		    BufferedReader(new InputStreamReader(client.getInputStream()));
-		String line = reader.readLine();
-		writer.println("You said: " + line);
+		PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+		InetAddress ip = client.getInetAddress();
+		String name = ip.getHostName();
+		String address = ip.getHostAddress();
+		//BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		//String line = reader.readLine();
+		writer.println("Hi " + name);
+		writer.println("Your IP address is " + address);
 		client.close();
 	    }
 	}
